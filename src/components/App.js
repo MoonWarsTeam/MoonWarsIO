dae6b01d33d5ba570daaadc47759603667d98268
Mark Napolitano <mark.napolitano@softwaresolutions802.com>
2021-07-06 20:15:43 -0400

    added basic main menu and server selection page to use as a template going forward

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,59 @@
 import './App.css';
 import Game from './Game';
+import MainMenu from './MainMenu';
+import ServerSelection from './ServerSelection';
+import { useState } from 'react';
+
+// todo - make these enums
+export const GameState = {
+  'MainMenu':0,
+  'ServerSelection':1,
+  'Game':2
+};
 
 function App() {
+
+  const [gameState, setGameState] = useState(GameState.MainMenu);
+
+  const updateGameState = (newState) => {
+    // handle logic
+    console.log("Setting Game State to: ", newState);
+    setGameState(newState);
+  }
+
+  const renderApplication = () => {
+    switch (gameState) {
+
+      case GameState.MainMenu:
+        return(
+          <MainMenu selectServer={() => updateGameState(GameState.ServerSelection)}/>
+        );
+      case GameState.Game:
+        return(
+          <div className="GameContainer">
+            <Game/>
+          </div>
+        );
+      case GameState.ServerSelection:
+        return(
+          <ServerSelection selectGameServer={() => updateGameState(GameState.Game)}/>
+        );
+      default:
+        console.log("Unhandled GameState: ", gameState);
+        return(
+          <MainMenu selectServer={() => updateGameState(GameState.ServerSelection)}/>
+        );
+    }
+  }
+
   return (
     <div className="App">
-      <div className="GameContainer">
-      <Game/>
-      </div>
+      {renderApplication()}
     </div>
   );
+
+
+  
 }
 
 export default App;
